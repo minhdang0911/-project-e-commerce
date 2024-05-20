5c0bf9234e65d32777ae634b6b86ba4b40f4ae0d
Minh Dang <minhdang9a8@gmail.com>
2024-05-20 19:18:06 +0700

    connect mongodb database and test api register

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 require('dotenv').config();
+const dbConnect = require('./config/dbconnect');
+const initRoutes = require('./routes');
 
 const app = express();
-const port = process.env || 8888;
+const port = process.env.PORT || 8888;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+dbConnect();
+initRoutes(app);
+
 app.use('/', (req, res) => {
     res.send('Server running');
 });
+
 app.listen(port, () => {
-    console.log('server running in port' + port);
+    console.log('server running on port: ' + port);
 });
