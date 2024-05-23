38a16888cb39e83f7dceaafa570e590fd6a8b75c
Minh Dang <minhdang9a8@gmail.com>
2024-05-23 18:54:59 +0700

    build api update blog

--- server/controller/blog.js
+++ server/controller/blog.js
@@ -11,6 +11,17 @@ const createNewBlog = asyncHandler(async (req, res) => {
     });
 });
 
+const updateBlog = asyncHandler(async (req, res) => {
+    const { bid } = req.params;
+    if (Object.keys(req.body).length === 0) throw new Error('Missing inputs');
+    const response = await Blog.findByIdAndUpdate(bid, req.body, { new: true });
+    return res.status(200).json({
+        success: response ? true : false,
+        updatedBlog: response ? response : 'Cannot update new blogs ',
+    });
+});
+
 module.exports = {
     createNewBlog,
+    updateBlog,
 };
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -3,5 +3,6 @@ const ctrls = require('../controller/blog');
 const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken');
 
 router.post('/', [verifyAccessToken, isAdmin], ctrls.createNewBlog);
+router.put('/:bid', [verifyAccessToken, isAdmin], ctrls.updateBlog);
 
 module.exports = router;
