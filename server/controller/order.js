a15c9082361b0068d23b177312e4bdfb2278ed4d
Minh Dang <minhdang9a8@gmail.com>
2024-05-24 21:12:52 +0700

    build api get order by user and get alls order

--- server/controller/order.js
+++ server/controller/order.js
@@ -1,5 +1,6 @@
 const Order = require('../models/order');
 const User = require('../models/user');
+const Coupon = require('../models/coupon');
 const asyncHandler = require('express-async-handler');
 
 const createOrder = asyncHandler(async (req, res) => {
@@ -17,11 +18,16 @@ const createOrder = asyncHandler(async (req, res) => {
         return sum + el.product.price * el.quantity;
     }, 0);
 
+    const createData = { products, total, orderBy: _id };
+
     if (coupon) {
-        total = Math.round((total * (1 - coupon / 100)) / 1000) * 1000;
+        const selectedCoupon = await Coupon.findById(coupon);
+        total = Math.round((total * (1 - +selectedCoupon?.discount / 100)) / 1000) * 1000 || total;
+        createData.total = total;
+        createData.coupon = coupon;
     }
 
-    const rs = await Order.create({ products, total, orderBy: _id });
+    const rs = await Order.create(createData);
 
     return res.status(200).json({
         success: rs ? true : false,
@@ -29,6 +35,39 @@ const createOrder = asyncHandler(async (req, res) => {
     });
 });
 
+const updateStatusOrder = asyncHandler(async (req, res) => {
+    const { oid } = req.params;
+    const { status } = req.body;
+    if (!status) throw new Error('Missing status');
+
+    const response = await Order.findByIdAndUpdate(oid, { status }, { new: true });
+
+    return res.status(200).json({
+        success: response ? true : false,
+        createdBrand: response ? response : 'Something went wrong',
+    });
+});
+
+const getUserOrder = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    const response = await Order.find({ orderBy: _id });
+    return res.status(200).json({
+        success: response ? true : false,
+        response: response ? response : 'Something went wrong',
+    });
+});
+
+const getAllOrder = asyncHandler(async (req, res) => {
+    const response = await Order.find();
+    return res.status(200).json({
+        success: response ? true : false,
+        response: response ? response : 'Something went wrong',
+    });
+});
+
 module.exports = {
     createOrder,
+    updateStatusOrder,
+    getUserOrder,
+    getAllOrder,
 };
--- server/routes/order.js
+++ server/routes/order.js
@@ -4,4 +4,8 @@ const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken');
 
 router.post('/', verifyAccessToken, ctrls.createOrder);
 
+router.get('/', verifyAccessToken, ctrls.getUserOrder);
+router.get('/admin', verifyAccessToken, isAdmin, ctrls.getAllOrder);
+router.put('/status/:oid', verifyAccessToken, isAdmin, ctrls.updateStatusOrder);
+
 module.exports = router;
