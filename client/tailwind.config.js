91a05b472fdac992c91519e71ae76ccc3531828c
Minh Dang <minhdang9a8@gmail.com>
2024-05-27 12:51:39 +0700

    setup Front-End

---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    theme: {
+        extend: {
+            width: {
+                main: '1220px',
+            },
+            backgroundColor: {
+                main: '#ee3131',
+            },
+            text: {
+                main: '#ee3131',
+            },
+            fontFamily: {
+                main: ['Poppins', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
